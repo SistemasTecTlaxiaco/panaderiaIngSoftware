c5b01a6ebe5cd9a80528f4501c73622dd12f996d
marcialmiguelrojas@gmail.com <w1gu3lr0j45m4rc1@gmail.com>
2020-05-21 22:23:26 -0500

    Creacion del menu

--- view/home.php
+++ view/home.php
@@ -3,19 +3,25 @@
 <?php include 'partials/header.php' ?>
 <nav class="navbar navbar-light" he  style="background-color: #EEEB2B">
         <a href="../index.php"><img id=logo1 src="../img/logo.png"></a>
+          <form class="form-inline my-2 my-lg-0">
+        </form>
+        <button class="btn btn-outline-primary"  onclick="location.href='../index.php'">Salir</button>
 </nav>
-<div class="container">
-        <div class="row mt-3 justify-content-md-center">
-            <div class="col-md-6">
-                <h1>Hola, bienvenido</H1>              
-            </div>
-
-            </div>
-                <div class="row mt-3 justify-content-md-center">
-                    <form action="" method="POST">
-                    
-                    </form>
-                </div>
+<div class="modal-dialog text-center">
+  <div class="col-sm-16">
+    <div class="modal-content">
+      <br>  <br>
+      <button  onclick="location.href=''" class="btn btn-outline-light text-dark" ><img id="imgTamaño" src="../img/img01.png" class="img-responsive" alt="Responsive image">Productos</button>
+      <br>  <br>
+      <button onclick="location.href=''" class="btn btn-outline-light text-dark" ><img id="imgTamaño" src="../img/img02.png" class="img-responsive" alt="Responsive image">Pedido</button>
+      <br>  <br>
+       <button onclick="location.href=''" class="btn btn-outline-light text-dark" ><img id="imgTamaño" src="../img/img03.png" class="img-responsive" alt="Responsive image">Acerca de Nosotros</button>
+      <br>  <br>
+     
     </div>
+  
+  </div>
+
+</div>
 
 <?php include 'partials/footer.php'; ?>
