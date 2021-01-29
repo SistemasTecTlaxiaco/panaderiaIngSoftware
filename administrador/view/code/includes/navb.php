5731fcdc7dbf912ee0422d86fa82cbd72430b011
Irene-95 <64617146+Irene-95@users.noreply.github.com>
2021-01-28 22:55:36 -0600

    codigo para la parte superior de la ventana principal del administrador

---
+++ administrador/view/code/includes/navb.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a href="javascript:location.reload()" title="Haz click en la imagen"><img id=logo1 src="view/code/img/logo.png"></a>
+  <a class="navbar-brand" href="javascript:location.reload()">Productos</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="javascript:location.reload()">Inicio <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#" data-toggle="modal" data-target="#add">Agregar Producto</a>
+      <a class="nav-item nav-link" href="#" onclick="actualizar();">Lista de Productos</a>
+    </div>
+  </div>
+</nav>
