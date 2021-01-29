694bb389dfa8419e04971439860a3b5edc643829
Irene-95 <64617146+Irene-95@users.noreply.github.com>
2021-01-29 13:29:34 -0600

    index.php add producto

---
+++ administrador/index.php
@@ -0,0 +1,6 @@
+<?php  require_once ('view/code/includes/header.php');?>
+
+
+<?php include 'view/code/list/list.php'; ?>
+ <?php  require_once ('view/code/includes/footer.php');?>
+  <script src="view/code/js/addp.js"></script>
