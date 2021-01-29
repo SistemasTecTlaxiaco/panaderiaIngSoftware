0cf7c1553e4a6743633ac397c6b0975ba7f6b25f
Irene-95 <64617146+Irene-95@users.noreply.github.com>
2021-01-28 22:46:21 -0600

    campos que se deben rellenar al agregar un producto al sistema

--- administrador/view/code/includes/modals.php
+++ administrador/view/code/includes/modals.php
@@ -25,3 +25,30 @@
 </div>
 </div>
             </div>
+            
+            <div class="col-md-7">
+              <div class="form-group has-feedback">
+
+              <input type="text" name="nombre" id="nombre" class="form-control" autofocus placeholder="Nombre del producto" required autocomplete="off" title=" Minimo 6, Maximo 50" minlength="6" maxlength="50"><span class="help-block"></span></div>
+
+             <div class="form-group has-feedback">
+               <input type="number" name="precio" id="precio" class="form-control" min="1" max="20" autofocus placeholder="Precio" required autocomplete="off" title="1 Minimo , Maximo 50">
+               <span class="help-block"></span></div>
+
+                <div class="form-group has-feedback">
+               <select id="tamano" name="tamano" required autofocus class="form-control" autocomplete="off">
+                 <option value="" selected>Elige el Tamaño</option>
+                 <option value="Grande">Grande</option>
+                 <option value="Mediano">Mediano</option>
+                 <option value="Chico">Chico</option>
+               </select>
+               <span class="help-block"></span></div>
+
+              <div class="form-group has-feedback">
+               <textarea type="text" name="descripcion" id="descripcion" class="form-control" autofocus placeholder="Descripción" required autocomplete="off" title="5 Minimo , Maximo 1000" minlength="5" maxlength="500"></textarea>
+               <span class="help-block"></span></div>  
+           </div>
+           </div>
+            <div class="col-md-2 mb-2">
+            </div>
+          </div>
