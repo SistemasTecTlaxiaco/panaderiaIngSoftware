bf6cb02ef387a38f9a7a257da209839154d99020
Irene-95 <64617146+Irene-95@users.noreply.github.com>
2021-02-01 01:48:44 -0600

    codigo para mostrar la lista de pedidos que tiene el administrador

--- administrador/view/code/list/list.php
+++ administrador/view/code/list/list.php
@@ -45,7 +45,35 @@
 		</div>		
     </div>
   </article>
+  </div>
 </div>
 </div>
+ </div>
+
+ <div id="contenidopedido" style="display: none">
+ <div class="container mt-5" align="center">
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="javascript:location.reload()">Inicio</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Lista de Pedidos</li>
+  </ol>
+</nav>
 </div>
+  <div class="container">
+ 
+      <div class="row d-flex justify-content-around col-md-offset-1 " >
+      
+    </div>
+     
+        <div class="row d-flex justify-content-around mt-1">
+            
+            <div class="card col-md-12  bg-light">
+            <article class="card-body">
+               <button type="submit" style="position: absolute;right: 50px;" class="btn btn-info btn-xs" onclick="actualizar();">Actualizar Lista</button>
+               <br>
+<div class="row" >
+    <div id="cuadro2" class="col-sm-12 col-md-12 col-lg-12">
+    <div class="col-sm-offset-1 col-sm-8" >
+        <h3 class="text-center"> <small class="mensaje"></small></h3>
+      </div>
  
